c7961a3197337b8ecec02054173b7305694686ac
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-12-02 14:47:46 +0100

    simple list with some navigation

---
+++ SimpleListsAndNavigation/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  SimpleListsAndNavigation
+//
+//  Created by Fabian on 02.12.20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 12 mini", "iPhone 12 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
--- SimpleListsAndNavigation/SimpleListsAndNavigationApp.swift
+++ SimpleListsAndNavigation/SimpleListsAndNavigationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SimpleListsAndNavigationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandmarkList()
         }
     }
 }
