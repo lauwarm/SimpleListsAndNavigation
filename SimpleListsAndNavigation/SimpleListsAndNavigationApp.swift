7af0e1932433cb30f6b86e8e235e717be64c5930
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-12-02 10:35:35 +0100

    Initial Commit

---
+++ SimpleListsAndNavigation/SimpleListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListsAndNavigationApp.swift
+//  SimpleListsAndNavigation
+//
+//  Created by Fabian on 02.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
